de3f93c05c5b45fe1155b508e9bf0dde914e5b85
Heather Krieger <heather.a.krieger@gmail.com>
2018-06-15 19:19:46 -0400

    attemption graph4 html

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from bokeh.models.widgets import TextInput #Select
 from bokeh.plotting import figure, show #, output_file
 from bokeh.models import Band, ColumnDataSource, Range1d, Title
 from bokeh.embed import components
+
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 #setting api key to access data from quandl
 q.ApiConfig.api_key = "WTAu2SBBZyADGzrpPL-c"
 
@@ -15,6 +18,7 @@ q.ApiConfig.api_key = "WTAu2SBBZyADGzrpPL-c"
     #return data
 
 def create_stock_plot(ti):
+    #getting stock data from ticker input
     data = q.get_table('WIKI/PRICES', paginate=True,
                    ticker = ti, date = { 'gte': '2018-01-01', 'lte': '2018-04-01' },
                    qopts={"columns":["ticker", "date", "close", "low", "high"]})
@@ -24,8 +28,8 @@ def create_stock_plot(ti):
 
     t1 = "Closing Cost with High/Low Reach Band"
     t2 = "From %s to %s" %('Jan 2018', 'Apr 2018')
-    #figure configuration
-    p = figure(title = t1, plot_width=500, plot_height=500, x_axis_type="datetime")
+    #creating figure
+    p = figure(title = t1, plot_width=700, plot_height=500, x_axis_type="datetime")
     p.y_range = Range1d(data.low[data.low.idxmin()]-2, data.high[data.high.idxmax()]+2)
     p.xaxis.axis_label = 'Days'
     p.yaxis.axis_label = 'Value in USD'
@@ -37,6 +41,7 @@ def create_stock_plot(ti):
 
     p.add_layout(band)
     p.add_layout(Title(text=t2, align="center"), "below")
+
     return p
 
 app = Flask(__name__)
@@ -50,11 +55,19 @@ def graph():
     text = request.form.get('stock-ticker')
     ti = text.upper()
 
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
     plot = create_stock_plot(ti)
     script, div = components(plot)
 
+    html = render_template('graph4.html', stock_ticker=ti, plot_script=script, plot_div=div,
+        js_resources=js_resources, css_resources=css_resources)
+
     if ti:
-        return render_template('graph3.html', stock_ticker=ti, script=script, div=div)
+        return encode_utf8(html)
+        #return render_template('graph3.html', stock_ticker=ti, script=script, div=div)
     else:
         return 'Please enter a valid stock ticker', 400
 
